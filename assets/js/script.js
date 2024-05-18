9a2f713787d8d70f0cca19abce829782add365ed
Sayali <sayalipanch17@gmail.com>
2024-05-18 19:57:34 +0530

    Added popup

--- assets/js/script.js
+++ assets/js/script.js
@@ -61,3 +61,27 @@ const activeElem = function () {
 }
 
 addEventOnElem(window, "scroll", activeElem);
+
+// preview course
+let preveiwContainer = document.querySelector(".course-preview");
+let previewBox = preveiwContainer.querySelectorAll(".preview");
+
+document.querySelectorAll(".course .course-card").forEach((course) => {
+  course.onclick = () => {
+    preveiwContainer.style.display = "flex";
+    let name = course.getAttribute("data-name");
+    previewBox.forEach((preview) => {
+      let target = preview.getAttribute("data-target");
+      if (name == target) {
+        preview.classList.add("active");
+      }
+    });
+  };
+});
+
+previewBox.forEach((close) => {
+  close.querySelector(".fa-times").onclick = () => {
+    close.classList.remove("active");
+    preveiwContainer.style.display = "none";
+  };
+});
